f77ed9871d657a8fcb03ed9027574ef6daef138a
Tomi Javanainen <tomi.javanainen@gmail.com>
2024-10-11 18:45:37 +0300

    Suppress logging from llm_api

--- settings.py
+++ settings.py
@@ -1,3 +1,8 @@
+import logging
+
 import llm_api
+
+llm_logger = logging.getLogger("llm_api")
+llm_logger.setLevel(logging.ERROR)
 
 LLM_API = llm_api.OllamaApi(model="llama3.1:8b", base_url="http://localhost:11434/v1")
