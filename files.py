b2bb7fca921827e53c7459137849102f51d7f411
Tomi Javanainen <tomi.javanainen@gmail.com>
2024-10-11 17:46:09 +0300

    Add file reader

---
+++ files.py
@@ -0,0 +1,18 @@
+from pathlib import Path
+from typing import Generator, NamedTuple
+
+
+class FileNameAndContent(NamedTuple):
+    filename: str
+    content: str
+
+
+def file_contents(directory: str) -> Generator[FileNameAndContent, None, None]:
+    """
+    Yields the contents of all files in the given directory.
+    """
+    path = Path(directory)
+    for file in path.iterdir():
+        if file.is_file():
+            with file.open("r", encoding="utf-8") as f:
+                yield FileNameAndContent(file.name, f.read())
